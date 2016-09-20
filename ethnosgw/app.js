60b9e713164d1d36330813a7425ab1ba1cd69e18
Brownkevg <brownkevg@gmail.com>
2016-09-20 11:37:57 -0400

    bootstrap and jquery added

--- ethnosgw/app.js
+++ ethnosgw/app.js
@@ -22,6 +22,11 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+//bootstrap jquery
+app.use('/js', express.static(__dirname + '/node_modules/bootstrap/dist/js'));
+app.use('/js', express.static(__dirname + '/node_modules/jquery/dist'));
+app.use('/css', express.static(__dirname + '/node_modules/bootstrap/dist/css'));
+
 app.use('/', routes);
 app.use('/users', users);
 
