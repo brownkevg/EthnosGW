f062d5249eeda828addd0fdb09993600ce533663
Brownkevg <brownkevg@gmail.com>
2016-10-09 20:32:38 -0400

    testing mongodb

--- ethnosgw/routes/index.js
+++ ethnosgw/routes/index.js
@@ -10,4 +10,15 @@ router.get('/search', function(req, res, next) {
 	res.render('search', {title: 'Ethnos'})
 })
 
+/* mongodb */
+router.get('/userlist', function(req,res){
+	var db = req.db;
+	var collection = db.get('usercollection');
+	collection.find({},{},function(e,docs){
+		res.render('userlist',{
+			"userlist" : docs
+		});
+	});
+});
+
 module.exports = router;
