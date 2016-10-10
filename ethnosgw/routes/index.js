da4271e6c1f9273643af2bbd6b64b94ede9e031c
awrobinson4 <awrobinson4@liberty.edu>
2016-10-09 20:34:08 -0400

    Finished upload page without dropdown boxes

--- ethnosgw/routes/index.js
+++ ethnosgw/routes/index.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Ethnos' });
 });
 
+router.get('/upload', function(req, res, next) {
+	res.render('upload', {title: 'Upload'});
+});
+
 module.exports = router;
