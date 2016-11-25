068a534d03cdd8db2c806a4c3815044bef127dce
JDPMan <justin@davidpritchard.com>
2016-11-25 18:46:54 -0500

    Enhanced homepage rendering

--- ethnosgw/pages/controller.js
+++ ethnosgw/pages/controller.js
@@ -5,11 +5,11 @@ exports.home = function(req,res){
 	// debugger;
 	var pageModel = {};
 	var searchObj = {};
-	pageCache.getMongo("media",searchObj,{mongoClient:dbClient,limit:1},function(results1){
+	pageCache.getMongo("media",searchObj,{mongoClient:dbClient, limit:10},function(results1){
 		// debugger;
 		pageModel.popular = results1;
 		searchObj = {};
-		pageCache.getMongo("media",searchObj,{mongoClient:dbClient,limit:1},function(results2){
+		pageCache.getMongo("media",searchObj,{mongoClient:dbClient, limit:10},function(results2){
 			pageModel.recommended = results2
 			res.render('home',{pageModel:pageModel});
 		})
