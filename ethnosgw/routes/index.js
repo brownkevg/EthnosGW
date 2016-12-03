efe0eb6fe81397722a1949a413da3fb9db470f08
brownkevg <brownkevg@gmail.com>
2016-12-03 18:27:47 -0500

    very impratical submit form
    Just added in how I submited a file into the bucket. Now Just need to
    combined both of the posts into one.

--- ethnosgw/routes/index.js
+++ ethnosgw/routes/index.js
@@ -1,5 +1,14 @@
 var express = require('express');
 var router = express.Router();
+// AWS
+var fs = require('fs');
+var S3FS = require('s3fs');
+var s3fsImpl = new S3FS('ethnosgw', {signatureVersion: 'v4', region: 'us-east-2'});
+var multiparty = require('connect-multiparty'),
+multipartyMiddleware = multiparty();
+router.use(multipartyMiddleware);
+var bucketName = 'ethnosgw';
+// end AWS
 
 /* GET home page. */
 // router.get('/', function(req, res, next) {
@@ -29,5 +38,20 @@ router.get('/userlist', function(req,res){
 		});
 	});
 });
+
+/* AWS bucket */
+router.post('/testPost', function(req, res){
+	console.log(req.files.uploadFile);
+	var file = req.files.uploadFile;
+	var stream = fs.createReadStream(file.path);
+	return s3fsImpl.writeFile(file.originalFilename, stream).then(function(){
+		fs.unlink(file.path, function(err){
+			if(err)
+				console.error(err);
+		})
+		res.redirect('/upload');
+	});
+});
+/* end AWS bucket */
 
 module.exports = router;
