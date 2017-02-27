bbc2a3ac7b60ebb9e59ab1d9be8e692085c11260
brownkevg <brownkevg@gmail.com>
2017-02-27 15:06:08 -0500

    upload
    added success page and upload4 for addional files(not linked to DB yet)

--- upload/controller.js
+++ upload/controller.js
@@ -14,7 +14,8 @@ exports.uploadContent = function(req,res){
 	content.route = req.body.title.replace(/ /g,'-').toLowerCase();
 	//Will need to check for the uniqueness of a route
 	dbClient.collection('media').save(content,function(err, result){
-		res.status(200).end(JSON.stringify({success:true}));
+		res.render('upload4');
+		// res.status(200).end(JSON.stringify({success:true}));
 	})
 }
 
